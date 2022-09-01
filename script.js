d5bbc689fc38a03af95f9adde47cacf929125ad0
jwesa <layoshicity@gmail.com>
2022-09-01 16:49:04 +0300

    Added responsivity

--- script.js
+++ script.js
@@ -1,68 +1,68 @@
 const progressBar = document.querySelector(".circular-progress");
-const valueInput = document.querySelector("#value-input");
-const animateSwitch = document.querySelector("#animated-switch");
-const hideSwitch = document.querySelector("#hide-switch");
+const valueInput = document.querySelector("#value__input");
+const animateSwitch = document.querySelector("#animate__switch");
+const hideSwitch = document.querySelector("#hide__switch");
 
 let progressValue = 0;
 let progressEndValue = 100;
-let toggle = false;
+let toggled = false;
 
-function increaseValueByOne() {
+const increaseValueByOne = () => {
     progressValue++;
     progressBar.style.background = `conic-gradient(
           #015afb ${progressValue * 3.6}deg,
           #eef2f5 ${progressValue * 3.6}deg
       )`;
-}
+};
 
-function setValue(e, speed) {
+const setValue = (e, speed) => {
     if (e.target.value.match("^([1-9][0-9]?|100)$")) {
         if (e.target.value <= progressValue) {
             progressValue = 0;
         }
-        const progress = setInterval(() => {
-            increaseValueByOne();
+		const progress = setInterval(() => {
+			increaseValueByOne();
             if (progressValue == e.target.value) {
                 clearInterval(progress);
             }
         }, speed);
     }
-}
+};
 
-function toggleAnimate(speed) {
+const toggleAnimate = (speed) => {
     const progress = setInterval(() => {
-        if (!toggle) {
+        if (!toggled) {
             clearInterval(progress);
+        }
+        if (progressValue >= progressEndValue && toggled === true) {
+            clearInterval(progress);
+            progressValue = 0;
+            toggleAnimate(speed);
         }
         valueInput.value = progressValue;
         increaseValueByOne();
-        if (progressValue >= progressEndValue && toggle === true) {
-            clearInterval(progress);
-            progressValue = 0;
-            progressEndValue = 100;
-            toggleAnimate(speed);
-        }
     }, speed);
-}
+};
 
-function toggleHide() {
-	if (progressBar.className === "circular-progress__hidden") {
-		progressBar.className = "circular-progress";
-		valueInput.disabled = false;
+const toggleHide = () => {
+	if (progressBar.style.visibility === "hidden") {
+        progressBar.style.visibility = "visible";
+        valueInput.disabled = false;
+        animateSwitch.disabled = false;
     } else {
-		progressBar.className = "circular-progress__hidden";
-		valueInput.disabled = true;
+        progressBar.style.visibility = "hidden";
+        valueInput.disabled = true;
+        animateSwitch.disabled = true;
     }
-}
+};
 
-valueInput.addEventListener("change", (event) => {
-	setValue(event, 15);
+valueInput.addEventListener("change", (e) => {
+    setValue(e, 15);
 });
 
 animateSwitch.addEventListener("click", () => {
-    toggle = !toggle;
+    toggled = !toggled;
     toggleAnimate(15);
 });
 
 hideSwitch.addEventListener("click", toggleHide);
-
